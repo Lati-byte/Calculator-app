3c3bd9a81b4bee4c1fdd1b78cf7cdf92b3634542
Lati-byte <harmony.rams@gmail.com>
2024-05-08 09:25:17 +0200

    added another function
    Added a function to perform arithmetic operation based on operator

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-let num1 = 0;
-let num2 = 0;
-let answer = 0;
+let num1 = 4;
+let num2 = 4;
+let operator = '-';
+
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return "Invalid operator";
+    }
+}
+
+// Test the operate function
+console.log(operate(operator, num1, num2)); // Output: 8
 
 // Function to add two numbers
 function add(a, b) {
